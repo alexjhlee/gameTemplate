477d403d8240abb62da3d73502f004e41e7478bf
alexjhlee <200237@hkis.edu.hk>
2015-12-09 10:14:16 +0800

    Change buttons

--- gamedev.js
+++ gamedev.js
@@ -1,48 +1,59 @@
 $( document ).ready(function() {
 
-	$("#goRight1").click(function() {
+	$("#goBack1").click(function() {
 		$("#exploreBody").css("background-image", "url('img/exploreRight1.jpg')");
-		$("#goRight1").hide();
+		$("#goBeach").hide();
 		$("#goBack").hide();
-		$("#goRight-Straight1").show();
-		$("#goRight-Right2").show();
+		$("#goForest2").show();
+		
 	});
 
-	$("#goRight-Straight1").click(function() {
+	$("#goForest2").click(function() {
 		$("#exploreBody").css("background-image", "url('img/exploreRight2.jpg')");
-		$("#goRight-Straight1").hide();
-		$("#goRight-Straight2").show();
-	});
+		$("#goBack1").show();
+		$("#goForest1").show();
+	});  
 
-	$("#goRight-Straight2").click(function() {
+	$("#goForest1").click(function() {
 		$("#exploreBody").css("background-image", "url('img/exploreRight3.jpg')");
-		$("#goRight-Straight2").hide();
-		$("#goRight-Straight3").show();
+		$("#goForest2").hide();
+		$("#goHouse").show();
+		$("#goBack2").show();
+		$("#goCave").show();
 	});
 
-	$("#goRight-Straight3").click(function() {
+		$("#goCave").click(function() {
 		$("#exploreBody").css("background-image", "url('img/exploreRight3.jpg')");
-		$("#goRight-Straight3").hide();
-		$("#goRight-Right2").show();
-	});
-
-	$("#goRight-Right2").click(function() {
-		$("#exploreBody").css("background-image", "url('img/explore2-goBack2.jpg')");
-		$("#goRight-Right2").hide();
+		$("#goForest1").hide();
 		$("#goBack").show();
+
+	$("#goHouse").click(function() {
+		$("#exploreBody").css("background-image", "url('img/exploreRight3.jpg')");
+		$("#goForest1").hide();
+		$("#goLivingroom").show();
+		$("#goBack3").show();
+		
+	});
+
+	$("#goLivingroom").click(function() {
+		$("#exploreBody").css("background-image", "url('img/explore2-goBack2.jpg')");
+		$("#House").hide();
+		$("#goKitchen").show();
+		$("#goRoom").show();
+		$("#goBack4").show();
 	});
 
 
-	$("#goLeft1").click(function() {
+	$("#goKitchen").click(function() {
 		$("#exploreBody").css("background-image", "url('img/exploreLeft1.jpg')");
-		$("#goLeft1").hide();
-		$("#goLeft2").show();
+		$("#goLivingroom").hide();
+		$("#goGoback5").show();
 	});
 
-	$("#goLeft-Straight1").click(function() {
+	$("#goRoom").click(function() {
 		$("#exploreBody").css("background-image", "url('img/exploreLeft2.jpg')");
-		$("#goLeft2").hide();
-		$("#goLeft3").show();
+		$("#goRoom").hide();
+		$("#goBack6").show();
 	});
 
 	$("#goLeft-Straight2").click(function() {
